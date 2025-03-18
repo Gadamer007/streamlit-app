d9c12e7ce8bcbbbe4d07065f1d5e49518f73cb6c
Gadamer007 <dmlandholm@gmail.com>
2025-03-18 10:16:26 +0100

    Update app.py

--- app.py
+++ app.py
@@ -168,7 +168,7 @@ def create_scatter_plot(data, reference_country):
     )
     fig.update_layout(
     height=500,  # Reduce height slightly
-    width=900,  # Increase width for a more horizontal shape
+    width=1200,  # Increase width for a more horizontal shape
 
         legend=dict(
             title=dict(font=dict(color="white")),
@@ -190,5 +190,4 @@ def create_scatter_plot(data, reference_country):
 # Display the plot
 scatter_plot = create_scatter_plot(data, reference_country)
 st.plotly_chart(scatter_plot, use_container_width=True)
-st.empty() 
 
