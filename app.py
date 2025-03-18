b10f5e9eb2914e2436a6e8fde305d6ec554a589c
Gadamer007 <dmlandholm@gmail.com>
2025-03-18 10:10:44 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-st.set_page_config(layout="long")
 
 # Title for your Streamlit app
 st.title("Top Countries for Financial Independence")
